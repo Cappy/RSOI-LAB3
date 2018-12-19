4e1b21fd263d2a9f1791136f129e4cc628592002
Pavel Kiselev <cappyru@gmail.com>
2018-12-19 06:06:02 +0300

    get-oauth2-token

--- ui-angular/src/app/account/account.component.ts
+++ ui-angular/src/app/account/account.component.ts
@@ -1,15 +1,18 @@
 ﻿import { Component, OnInit } from '@angular/core';
 import { first } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
 
 import { User } from '../_models';
+import { Token } from '../_models';
 import { UserService } from '../_services';
 
 @Component({templateUrl: 'account.component.html'})
 export class AccountComponent implements OnInit {
     currentUser: User;
     users: User[] = [];
+	OAuth2Token: string;
 
-    constructor(private userService: UserService) {
+    constructor(private userService: UserService, private http: HttpClient) {
         this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
     }
 
@@ -22,6 +25,12 @@ export class AccountComponent implements OnInit {
             this.loadAllUsers() 
         });
     }
+	
+	getToken(currentUser: User) {
+		return this.http.post<Token>(`/api/auth/get-oauth2-token`, currentUser).subscribe(OAuth2Token => {
+			this.OAuth2Token = OAuth2Token.token;
+		});
+	}
 
     private loadAllUsers() {
         this.userService.getAll().pipe(first()).subscribe(users => { 
